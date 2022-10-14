1b2817e1486aa1c0d0f892323d9b7a4ed84f8a1f
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-10-15 00:32:03 +0530

    def rev(a):     s=0     while a>0:         rem=a%10         s=(s*10)+rem         a//=10     return s a=int(input()) p1=0 p2=0 d1=0 d2=0 for i in range(a-1,0,-1):     if rev(i)==i:         p1=i         d1=a-i         break for i in range(a+1,10000):     if rev(i)==i:         p2=i         d2=i-a         break if d1==d2:     print(p1,p2) elif d1>d2:     print(p2) elif d1<d2:     print(p1)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,28 +1,28 @@
-import math
-x=int(input())
-k=0
-s=0
-for i in range(x+1,10000):
-    t=i
-    k=0
-    while i:
-        d=i%10
-        i=i//10
-        k=k*10+d
-    if(k==t):
-        break
-for i in range(x-1,1,-1):
-    z=i
+def rev(a):
     s=0
-    while i:
-        d=i%10
-        i=i//10
-        s=s*10+d
-    if(s==z):
+    while a>0:
+        rem=a%10
+        s=(s*10)+rem
+        a//=10
+    return s
+a=int(input())
+p1=0
+p2=0
+d1=0
+d2=0
+for i in range(a-1,0,-1):
+    if rev(i)==i:
+        p1=i
+        d1=a-i
         break
-if(abs(x-s)==abs(k-x)):
-    print(s,k)
-elif(abs(x-s)<abs(k-x)):
-    print(s)
-else:
-    print(k)
+for i in range(a+1,10000):
+    if rev(i)==i:
+        p2=i
+        d2=i-a
+        break
+if d1==d2:
+    print(p1,p2)
+elif d1>d2:
+    print(p2)
+elif d1<d2:
+    print(p1)
